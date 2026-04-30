0e7acb9a58da62d353257f1d6b4cc2376c240e18
FullPon <ryu1358nosuke@gmail.com>
2026-04-30 19:30:42 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const request = require('request');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('messageCreate', msg => {
+  if (msg.author.bot) return;
+
+  request.post({
+    uri: process.env.GAS_URL,
+    headers: { 'Content-Type': 'application/json' },
+    json: {
+      message: msg.content,
+      user: msg.author.username
+    }
+  });
+});
+
+client.login(process.env.DISCORD_TOKEN);
